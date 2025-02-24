23abf1bb26f1ce919f20e23ad37492ece231a292
AI314159 <104926797+AI314159@users.noreply.github.com>
2025-02-24 17:46:44 -0500

    Made the shell infinite, add exit command and better errors

--- src/lib.rs
+++ src/lib.rs
@@ -70,34 +70,43 @@ pub unsafe extern "C" fn kmain(multiboot_info_address: usize) -> ! {
         test_file.read_size()
     );
     test_file.write_contents();
+    // Print a newline
+    println!();
 
     // FIXME: This should be dynamically allocated.
     let mut buffer = [0u8; 128];
-    print!("\n>");
-
-    let n_chars = input::get_user_input(&mut buffer);
-    let inputted_string = core::str::from_utf8(&buffer[..n_chars]).unwrap().trim();
-
-    // So this is a woefully inelegant solution for running programs, and should be changed later.
-    // Ideally this will run through a "bin" folder till it hits the command inputted, and run that, like PATH would.
-    if inputted_string.starts_with("echo") {
-        // Find the first space after the command and trim out the command.
-        if let Some(space_index) = inputted_string.find(' ') {
-            let args = inputted_string[space_index + 1..].trim();
-            echo::echo(args);
-        } else {
-            echo::echo("");
-        }
-    } else {
-        println!("You entered {} chars: {}", n_chars, inputted_string);
-    }
-
 
     keyboard::map_key(0x01,  tab_handler::switch_tab);
 
-    // Infinite loop to keep the kernel running
     loop {
+        print!("> ");
+
         let n_chars = input::get_user_input(&mut buffer);
-        let inputted_string = core::str::from_utf8(&buffer).unwrap();
+        let inputted_string = core::str::from_utf8(&buffer[..n_chars]).unwrap().trim();
+
+        if inputted_string.is_empty() {
+            // Return to the start of the loop.
+            continue;
+        }
+
+        // So this is a woefully inelegant solution for running programs, and should be changed later.
+        // Ideally this will run through a "bin" folder till it hits the command inputted, and run that, like PATH would.
+        if inputted_string.starts_with("echo") {
+            // Find the first space after the command and trim out the command.
+            if let Some(space_index) = inputted_string.find(' ') {
+                let args = inputted_string[space_index + 1..].trim();
+                echo::echo(args);
+            } else {
+                echo::echo("");
+            }
+        }
+        else if inputted_string.starts_with("exit") {
+            println!("Exiting the GUIneu kernel-mode debug shell...");
+            break;
+        } else {
+            println!("GUIneu: Unknown command: {}", inputted_string);
+        }
     }
+    // Infinite loop to keep the kernel running
+    loop {}
 }
